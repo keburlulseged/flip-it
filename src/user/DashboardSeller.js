16d7b6b4ee460e3585d979e5224f1a2aea858149
Kebur Lulseged <kebur.lulseged@gmail.com>
2021-04-19 14:43:54 -0700

    set up display and account setup button for non-stripe users

--- src/user/DashboardSeller.js
+++ src/user/DashboardSeller.js
@@ -1,8 +1,51 @@
 import DashboardNav from "../components/DashboardNav";
 import ConnectNav from "../components/ConnectNav";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { HomeOutlined } from "@ant-design/icons";
 
 const DashboardSeller = () => {
+  const { auth } = useSelector((state) => ({ ...state }));
+  const connected = () => (
+    <div className="container-fluid p-4">
+      <div className="row">
+        <div className="col-md-10">
+          <h2>Your Hotes</h2>
+        </div>
+
+        <div className="col-md-2 ">
+          <Link to="/hotels/new" className="btn btn-primary">
+            {" "}
+            + Add New{" "}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+
+  const notConnected = () => (
+    <div className="container-fluid p-4">
+      <div className="row">
+        <div className="col-md-12 offest-md-3 text-center">
+          <div className="p-5 pointer">
+            <HomeOutlined className="h1" />
+            <h4>Setup payouts to post hotel rooms</h4>
+            <p className="lead">
+              We partner with stripe to transfer earnings to your bank account
+            </p>
+            <button className="btn btn-primary mb-3">Setup Payouts</button>
+            <p className="text-muted">
+              <small>
+                You'll be redirected to Stripe to complete the onboarding
+                process
+              </small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <>
       <div className="container-fluid bg-secondary p-5">
@@ -13,20 +56,14 @@ const DashboardSeller = () => {
         <DashboardNav />
       </div>
 
-      <div className="container-fluid p-4">
-        <div className="row">
-          <div className="col-md-10">
-            <h2>Your Hotes</h2>
-          </div>
+      {auth &&
+      auth.user &&
+      auth.user.stripe_seller &&
+      auth.user.stripe_seller.charges_enabled
+        ? connected()
+        : notConnected()}
 
-          <div className="col-md-2 ">
-            <Link to="/hotels/new" className="btn btn-primary">
-              {" "}
-              + Add New{" "}
-            </Link>
-          </div>
-        </div>
-      </div>
+      {/* <pre>{JSON.stringify(auth, null, 4)}</pre> */}
     </>
   );
 };

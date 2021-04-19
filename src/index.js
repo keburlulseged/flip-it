5c7a75fad48c2a84bad21b9758bc820e4f163e79
Kebur Lulseged <kebur.lulseged@gmail.com>
2021-04-19 13:12:02 -0700

    add user info and avatar in hero section of dashboard

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "antd/dist/antd.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from './reducers';
+import rootReducer from "./reducers";
 
 const store = createStore(rootReducer, composeWithDevTools());
 
